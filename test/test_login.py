459d98878d6eb80052ebe1561f36a5e5b9e75671
Nadya Kuzmina <nkzmn@mail.ru>
2025-06-08 16:59:41 +0300

    page added

--- test/test_login.py
+++ test/test_login.py
@@ -1,60 +1,24 @@
-import time
-import unittest
-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from pages.login_page import LoginPage
 
 URL = 'https://berpress.github.io/selenium-login-demo/'
+driver = webdriver.Chrome()
 
-class TestLogin(unittest.TestCase):
-
-    def test_add_valid_data(self):
-        # здесь позитивный тест (успешная авторизация с валидными данными)
-        # login: admin
-        # password: password
-
-        # открыть страницу
-        driver = webdriver.Chrome()
+def test_valid():
+        # open_site (driver=driver, url = URL)
         driver.get(URL)
+        login_page = LoginPage(driver)
+        login_page.add_login(name='admin', password='password>')
 
-        # находим все элементы на странице
-        name = driver.find_element(By.ID, 'username')
-        password = driver.find_element(By.ID, 'password')
-        btn_ok = driver.find_element(By.ID, 'login-btn')
-        result = driver.find_element(By.ID, 'result')
-
-        # выполняем действия и заполняем поля
-        name.send_keys('admin')
-        password.send_keys('password')
-        time.sleep(1)
-        btn_ok.click()
-        time.sleep(1)
+        assert login_page.result.textContent == 'Успешно! Вход выполнен.'
         driver.quit()
 
-        assert result.textContent == 'Успешно! Вход выполнен.'
 
-    def test_add_invalid_data(self):
-        # здесь негативный тест (должна быть неуспешная авторизация с ошибкой)
-        # login: ad
-        # password: pass
+def test_invalid():
+    # open_site (driver=driver, url = URL)
+    driver.get(URL)
+    login_page = LoginPage(driver)
+    login_page.add_login(name='ad', password='pass>')
 
-        # открыть страницу
-        driver = webdriver.Chrome()
-        driver.get(URL)
-
-        # находим все элементы на странице
-        name = driver.find_element(By.ID, 'username')
-        password = driver.find_element(By.ID, 'password')
-        btn_ok = driver.find_element(By.ID, 'login-btn')
-        result = driver.find_element(By.ID, 'result')
-
-
-        # выполняем действия и заполняем поля
-        name.send_keys('ad')
-        password.send_keys('pass')
-        time.sleep(1)
-        btn_ok.click()
-        time.sleep(1)
-        driver.quit()
-
-        assert result.textContent == 'Успешно! Вход выполнен.'
+    assert login_page.result.textContent == 'Успешно! Вход выполнен.'
+    driver.quit()
